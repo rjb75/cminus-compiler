de68dfb2e9833a5fa73f85918427683bb68ba390
Robert Brown <robert.brown1@ucalgary.ca>
2023-12-06 10:17:03 -0700

    a3: test: update test cases

--- analyzer-tester.sh
+++ analyzer-tester.sh
@@ -12,37 +12,31 @@ for f in ./tests/analyzer/*.cm; do
     COUNT=$((COUNT + 1))
     name=${f##*/}
     echo $name >> analyzer-test-err.log
-    rm "${f}.tmp.err"
+    rm "${f}.tmp.err" 2>> analyzer-test-err.log
     ./analyzer -o "${f}.tst.res" "$f" 2>"${f}.tmp.err"
     if [ -f  "${f}.tst.res" ]
     then
         diff "${f}.res" "${f}.tst.res"
         if [ $? -eq 0 ]
         then
-            echo -e "\033[0m"
-            echo "✅ Success for ${name}"
+            echo -e "\033[0m✅ Success for ${name}"
         else
-            echo -e "\033[0m"
-            echo "❌ Fail for ${name}"
+            echo -e "\033[0m❌ Fail for ${name}"
             FAILED=$((FAILED + 1)) 
         fi
     else
         diff "${f}.err" "${f}.tmp.err"
         if [ $? -eq 0 ]
         then
-            echo -e "\033[0m"
-            echo "✅ Success for ${name}"
+            echo -e "\033[0m✅ Success for ${name}"
         else
-            echo -e "\033[0m"
-            echo "❌ Fail for ${name}"
+            echo -e "\033[0m❌ Fail for ${name}"
             FAILED=$((FAILED + 1)) 
         fi
     fi
 done
 
-echo -e "\033[0m"
-
-echo "Ran ${COUNT} tests"
+echo -e "\033[0mRan ${COUNT} tests"
 
 if [ $FAILED -eq 0 ]
 then
